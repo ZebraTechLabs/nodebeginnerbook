329df7440f0c39b2537ad728e7b71ddd0e60533a
Raghav Atal <raghav.atal@gmail.com>
2013-08-05 05:06:01 +0000

    Done the right way

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ function start(route, handle){
 http.createServer(function(request, response){
     var pathname = url.parse(request.url).pathname;
     console.log("Request for "+ pathname + " received.");
-    var content = route(handle, pathname);
-    response.writeHead(200, {"Content-Type":"text/plain"});
-    response.write(content);
-    response.end();
+    route(handle, pathname, response);
+    
 }).listen(8080);
 
 
