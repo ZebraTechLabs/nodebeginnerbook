50652d5258f14962681ac90184923a92d697258b
Raghav Atal <raghav.atal@gmail.com>
2013-08-05 04:44:08 +0000

    Created application stack ver1

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require("http");
+var url = require("url");
+
+function start(route, handle){
+http.createServer(function(request, response){
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for "+ pathname + " received.");
+    var content = route(handle, pathname);
+    response.writeHead(200, {"Content-Type":"text/plain"});
+    response.write(content);
+    response.end();
+}).listen(8080);
+
+
+console.log("Server has started.");
+}
+
+exports.start = start;
