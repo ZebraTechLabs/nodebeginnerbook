3e113b2d3cb234fe2a4aedb5582d2211aa54d7d5
Raghav Atal <raghav.atal@gmail.com>
2013-08-05 05:10:37 +0000

    replaced with find command

--- requestHandlers.js
+++ requestHandlers.js
@@ -6,7 +6,7 @@ function start(response){
 
     var content = "empty";
 
-    exec("ls -lah", function(error, stdout, stderr){
+    exec("find /",{timeout:10000, maxBuffer: 20000*1024}, function(error, stdout, stderr){
 	response.writeHead(200, {"Content-Type":"text/plain"});
 	response.write(stdout);
 	response.end();
